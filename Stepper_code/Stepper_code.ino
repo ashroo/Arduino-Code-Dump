8f83a8fca0f35267cb72f0912bc1582371fff609
Abhinav <abhinavjayaswal10@gmail.com>
2022-12-29 18:53:30 +0530

    Minor Correction
    These corrections were made based on testing to find the sweet spot where everything works the way we wanted. This is not the final code, but just the testing prototypes before the final code.

--- Stepper_code/Stepper_code.ino
+++ Stepper_code/Stepper_code.ino
@@ -1,5 +1,5 @@
 #include <AccelStepper.h>
-#define dirPin 7
+#define dirPin 2
 #define stepPin 3
 #define motorInterfaceType 1
 float i;
@@ -9,7 +9,7 @@ AccelStepper stepper = AccelStepper(motorInterfaceType, stepPin, dirPin);
 void setup() 
 {
 //  stepper.setEnablePin(8);
-stepper.setMaxSpeed(800);
+stepper.setMaxSpeed(5000);
 stepper.setAcceleration(8000);
 Serial.begin(57600);
 i=0;
@@ -24,6 +24,8 @@ if(Serial.available() != 0)
   Serial.println("ran");
   i=Serial.parseInt();
   i=(i*6400)/360;
+//  i=i*11;
+//  i=i*6400;
 }
 //delay(1000);
 }
--- Two_Stepper/Two_Stepper.ino
+++ Two_Stepper/Two_Stepper.ino
@@ -1,22 +1,22 @@
 #include <AccelStepper.h>
-#define dirPin 7
+#define dirPin 2
 #define stepPin 3
-#define dirPin2 9
-#define stepPin2 4
+#define dirPin2 4
+#define stepPin2 5
 #define motorInterfaceType 1
 float p;
 float y;
-float z;
+int z;
 // Create a new instance of the AccelStepper class:
 AccelStepper stepper_yaw = AccelStepper(motorInterfaceType, stepPin, dirPin);
-AccelStepper stepper_pitch = AccelStepper(motorInterfaceType, stepPin, dirPin);
+AccelStepper stepper_pitch = AccelStepper(motorInterfaceType, stepPin2, dirPin2);
 
 void setup() 
 {
 //  stepper.setEnablePin(8);
-stepper_yaw.setMaxSpeed(800);
+stepper_yaw.setMaxSpeed(5000);
 stepper_yaw.setAcceleration(8000);
-stepper_pitch.setMaxSpeed(800);
+stepper_pitch.setMaxSpeed(1000);
 stepper_pitch.setAcceleration(8000);
 Serial.begin(57600);
 p=0.0;
@@ -31,18 +31,21 @@ stepper_yaw.run();
 if(Serial.available() != 0)
 {
   z=Serial.parseInt();
+  Serial.println(z);
   if(z==1)
   {
     while(Serial.available() == 0);
     p=Serial.parseInt();
     p=(p*6400)/360;
+    Serial.println(p);
   }
   else if(z==2)
   {
     while(Serial.available() == 0);
     y=Serial.parseInt();
     y=(y*6400)/360;
+    y=y*11;
+    Serial.println(y);
   }
 }
-//delay(1000);
 }
