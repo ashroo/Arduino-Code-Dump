f4a807a6b507d84a29d0bb3d5aec49f252269e8b
Abhinav Jayaswal <abhinavjayaswal10@gmail.com>
2023-09-22 14:52:04 +0530

    Adding INA219 to the Motor testing stand.
    - This update calculates and outputs Current and power used by the motor, and Voltage of the connected bus with the help of INA219

--- TT_Stand_code/TT_Stand_code.ino
+++ TT_Stand_code/TT_Stand_code.ino
@@ -1,37 +1,63 @@
+//Inclusion
 #include "HX711.h" //This library can be obtained here http://librarymanager/All#Avia_HX711
+#include <Wire.h>
+#include <Adafruit_INA219.h>
 
+//defination
 #define TORQUE_DOUT_PIN  PA8
 #define TORQUE_SCK_PIN  PB15
-
 #define THRUST_DOUT_PIN  PA7
 #define THRUST_SCK_PIN  PA6
 
-
+//declaration
 HX711 thrust;
 HX711 torque;
-
+Adafruit_INA219 ina219;
 float torque_cf = 2468.1077;
 float thrust_cf = -693.554;
 
 void setup() {
-  Serial.begin(9600);
-
+  Serial.begin(115200);
+  while (!Serial) 
+  {
+      delay(1);
+  }
   thrust.begin(THRUST_DOUT_PIN, THRUST_SCK_PIN);
   thrust.set_scale(thrust_cf);
   thrust.tare(); 
   torque.begin(TORQUE_DOUT_PIN, TORQUE_SCK_PIN);
   torque.set_scale(torque_cf);
   torque.tare(); 
+  if (! ina219.begin()) 
+  {
+    Serial.println("Failed to find INA219 chip");
+    //while (1) { delay(10); }
+  }
   Serial.println("----------Starting reading----------");
 }
 
 void loop() 
 {
+  float power_mW = 0;
+  float busvoltage = 0;
+  float current_mA = 0;
+
+  busvoltage = ina219.getBusVoltage_V();
+  current_mA = ina219.getCurrent_mA();
+  power_mW = ina219.getPower_mW();
+  
   Serial.print("Thrust=");
   Serial.print(thrust.get_units(), 2);
   Serial.print("g Torque=");
   Serial.print(torque.get_units(), 2);
   Serial.println(" g");
+
+  Serial.print("Bus Voltage:   "); Serial.print(busvoltage); 
+  Serial.print("V Current="); Serial.print(current_mA); 
+  Serial.print("mA Power="); Serial.print(power_mW); 
+  Serial.println("mW");
+  Serial.println("");
+  
   if(Serial.available())
   {
     char temp = Serial.read();
